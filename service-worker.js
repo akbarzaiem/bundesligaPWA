bea2b7ad39b89b6a562996497c2928a488949dd5
akbarzaiem <akbarzaiemp@gmail.com>
2020-11-14 14:57:48 +0700

    first upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+if (workbox)
+    console.log(`Workbox berhasil dimuat`);
+else
+    console.log(`Workbox gagal dimuat`);
+
+workbox.precaching.precacheAndRoute([
+    { url: '/', revision: '1' },
+    { url: '/nav.html', revision: '1' },
+    { url: '/index.html', revision: '1' },
+    { url: '/pages/home.html', revision: '1' },
+    { url: '/pages/saved.html', revision: '1' },
+    { url: '/pages/tim.html', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/js/materialize.js', revision: '1' },
+    { url: '/js/nav.js', revision: '1' },
+    { url: '/js/api.js', revision: '1' },
+    { url: '/js/idb.js', revision: '1' },
+    { url: '/image/bundes-logo.png', revision: '1' },
+    { url: '/image/football-badge.png', revision: '1' },
+    { url: '/image/star.png', revision: '1' },
+    { url: '/image/trophy.png', revision: '1' },
+    { url: '/icon1.png', revision: '1' },
+    { url: '/icon2.png', revision: '1' },
+    { url: '/manifest.json', revision: '1' }
+]);
+
+workbox.routing.registerRoute(
+    new RegExp('https://api.football-data.org/v2/'),
+    workbox.strategies.staleWhileRevalidate()
+);
